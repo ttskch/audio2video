608f18bd81a80c3f0409c193d47a4984f12f8a79
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2016-06-21 00:45:14 +0900

    just install ttskch/Silex-Skeleton

---
+++ web/index_dev.php
@@ -0,0 +1,26 @@
+<?php
+
+use Symfony\Component\Debug\Debug;
+
+// This check prevents access to debug front controllers that are deployed by accident to production servers.
+// Feel free to remove this, extend it, or make something more sophisticated.
+if (isset($_SERVER['HTTP_CLIENT_IP'])
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
+) {
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
+
+if (preg_match('/\.(png|jpg|jpeg|gif|css|js|map|pdf|ttf|woff|woff2)(\?.+)?$/', $_SERVER['REQUEST_URI'])) {
+    return false;
+}
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+Debug::enable();
+
+$app = require __DIR__.'/../src/app.php';
+require __DIR__.'/../config/dev.php';
+require __DIR__.'/../src/controllers.php';
+$app->run();
